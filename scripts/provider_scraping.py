a2c9463790ffd45594de7fa57596422aa0573d8a
Dan Knox <dan@scienceexchange.com>
2013-10-17 16:23:09 -0700

    Tweaks to stripHtmlTags function

--- scripts/provider_scraping.py
+++ scripts/provider_scraping.py
@@ -18,52 +18,50 @@ provider_urls = ["http://" + root + a["href"] for a in providers]
 
 # === Function to strip HTML tags (via http://stackoverflow.com/a/14603269/2874813) === #
 def stripHtmlTags(htmlTxt):
-	if htmlTxt is None:
-		return None
-	else:
-		return ''.join(BeautifulSoup(htmlTxt).find_all(text=True))
-
+    if not htmlTxt:
+        return ''.join(BeautifulSoup(htmlTxt).find_all(text=True))
+  
 
 # === Writes provider output to .csv file === #
 with open("data/providers.csv", "w") as f:
-	fieldnames = ("name", "summary", "location", "service_areas", "homepage_url", "service_urls")
-	output = csv.writer(f, delimiter=",")
-	output.writerow(fieldnames)
+    fieldnames = ("name", "summary", "location", "service_areas", "homepage_url", "service_urls")
+    output = csv.writer(f, delimiter=",")
+    output.writerow(fieldnames)
 
-	for url in provider_urls:
-		try:
-			soup = BeautifulSoup(urlopen(url).read())
+    for url in provider_urls:
+        try:
+            soup = BeautifulSoup(urlopen(url).read())
 
-			# === Get provider name === #
-			name_html = soup.h1.encode('utf-8')
-			name = stripHtmlTags(name_html)
-			print "Getting data for %s \n" % name
-			
-			# === Get provider summary === #
-			summary = soup.find_all(id="provider_description")
+            # === Get provider name === #
+            name_html = soup.h1.encode('utf-8')
+            name = stripHtmlTags(name_html)
+            print "Getting data for %s \n" % name
 
-			# === Get provider location === #
-			location_caption = soup.find(text="Headquarters")
-			location_html = location_caption.find_next("td").encode('utf-8')
-			location = stripHtmlTags(location_html)
+            # === Get provider summary === #
+            summary = soup.find_all(id="provider_description")
 
-			# === Get provider service areas === #
-			service_areas_caption = soup.find(text="Service Areas")
-			service_areas_html = service_areas_caption.find_next("td").encode('utf-8')
-			service_areas = stripHtmlTags(service_areas_html)
+            # === Get provider location === #
+            location_caption = soup.find(text="Headquarters")
+            location_html = location_caption.find_next("td").encode('utf-8')
+            location = stripHtmlTags(location_html)
 
-			# === Get provider URL === #
-			homepage_url_html = soup.find("a", class_="trackable", href=True).encode('utf-8')
-			homepage_url = stripHtmlTags(homepage_url_html)
-			
-			# == Get provider services == #
-			service_urls = [h3.a['href'] for h3 in soup.find_all('h3')]
+            # === Get provider service areas === #
+            service_areas_caption = soup.find(text="Service Areas")
+            service_areas_html = service_areas_caption.find_next("td").encode('utf-8')
+            service_areas = stripHtmlTags(service_areas_html)
 
-			output.writerow([name, summary, location, service_areas, homepage_url, service_urls])
-		
-		except:
-			pass
-		
-		sleep(1)
+            # === Get provider URL === #
+            homepage_url_html = soup.find("a", class_="trackable", href=True).encode('utf-8')
+            homepage_url = stripHtmlTags(homepage_url_html)
+
+            # == Get provider services == #
+            service_urls = [h3.a['href'] for h3 in soup.find_all('h3')]
+
+            output.writerow([name, summary, location, service_areas, homepage_url, service_urls])
+
+        except:
+            pass
+
+        sleep(1)
 
 print "Done writing file"
